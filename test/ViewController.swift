7055f2efe9779c5c82dd2662dcc8ed12d53a7af5
shunsukekyuwa <s.kyuwa9@gmail.com>
2016-07-16 23:10:10 +0900

    add tableViewCell

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -50,5 +52,11 @@ enum contentType {
         case .iCloud:
             return "iCloud"
         }
+    }
+}
+
+class TableViewCell: UITableViewCell {
+    func setCell(type: contentType){
+        textLabel?.text = type.title
     }
 }
