53c65622adb4bb8fe5b4d7c0007340dfb366f2f1
shunsukekyuwa <s.kyuwa9@gmail.com>
2016-07-17 00:06:20 +0900

    fix tableViewCell

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,34 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let viewModel: [contentType] = [contentType.Facebook, contentType.iCloud]
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        view.backgroundColor = UIColor.blackColor()
-
-        let baseImage = UIImage(named: "btn_usersearch_44x44pt_highlight")!
-        let capInsets: UIEdgeInsets = UIEdgeInsetsMake(5, 5, 5, 5.0)
-        var rectInsets: UIEdgeInsets = UIEdgeInsetsZero
-        rectInsets = UIEdgeInsetsMake(0, -80, 0, -20)
-
-        let btn = UIButton(frame: CGRect(x: 126, y: 200, width: 180, height: 88))
-        btn.setTitle("hogehoghodddddddddddddddddddd", forState: UIControlState.Normal)
-        btn.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        btn.titleLabel!.font = UIFont(name: "System", size: 18.0)
-
-        let resizeImage = baseImage.resizableImageWithCapInsets(capInsets, resizingMode: UIImageResizingMode.Tile).imageWithAlignmentRectInsets(rectInsets)
-        btn.setBackgroundImage(resizeImage, forState: UIControlState.Normal)
-
-
-        view.addSubview(btn)
+        tableView.registerNib(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        if let contentCell = cell as? TableViewCell {
+            contentCell.setCell(viewModel[indexPath.row])
+            contentCell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+            return contentCell
+        } else {
+            return UITableViewCell()
+        }
     }
 }
 
