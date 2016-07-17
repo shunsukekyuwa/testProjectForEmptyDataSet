9023b0fe16cf1f69496216ac34890213a792b0b4
shunsukekyuwa <s.kyuwa9@gmail.com>
2016-07-17 18:23:42 +0900

    string extension'

---
+++ test/String+extension.swift
@@ -0,0 +1,17 @@
+//
+//  String+extension.swift
+//  test
+//
+//  Created by 久和俊介 on H28/07/17.
+//  Copyright © 平成28年 kyuwa. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    func widthWithConstrainedHeight(height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: CGFloat.max, height: height)
+        let boundingBox = self.boundingRectWithSize(constraintRect, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        return boundingBox.width
+    }
+}
